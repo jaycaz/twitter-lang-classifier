0b348a2948717e2888057466cbefff8a2ea63bce
Martina Marek <martina@DN0a22a62b.SUNet>
2015-10-20 10:51:04 -0700

    added classifier interface

--- Language identification/src/classifier/BaselineClassifier.java
+++ Language identification/src/classifier/BaselineClassifier.java
@@ -1,5 +1,14 @@
 package classifier;
 
-public class BaselineClassifier {
+import java.util.ArrayList;
 
+public class BaselineClassifier implements Classifier {
+	public void train(ArrayList<ArrayList<String>> trainingSentences) {
+		
+	}
+	
+	public int classify(ArrayList<String> sentence) {
+		
+		return 0;
+	}
 }
