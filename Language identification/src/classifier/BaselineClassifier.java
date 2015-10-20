e105fee0ed5d3510e92bda2c3d6d2c61aaaae1b5
Martina Marek <martina@DN0a22a62b.SUNet>
2015-10-20 11:02:07 -0700

    added basic functionality to Baseline

--- Language identification/src/classifier/BaselineClassifier.java
+++ Language identification/src/classifier/BaselineClassifier.java
@@ -1,10 +1,13 @@
 package classifier;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class BaselineClassifier implements Classifier {
+	HashMap<String, String> mostFrequentWords;
+	
 	public void train(ArrayList<ArrayList<String>> trainingSentences) {
-		
+		mostFrequentWords = new HashMap<String, String> ();
 	}
 	
 	public int classify(ArrayList<String> sentence) {
