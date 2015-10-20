3cb3f7721cfdb66f5abfe89f53d2e4b94d934b73
jaycaz <jacazamias@gmail.com>
2015-10-20 15:00:13 -0700

    Add Language class

---
+++ Language identification/src/util/Language.java
@@ -0,0 +1,49 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* class util.Language
+
+    Stores the language codes and plaintext language names
+ */
+public class Language {
+
+    // Plaintext language name
+    public String name;
+
+    // ISO 639-3 language code name
+    public String code;
+
+    public static final String UNKNOWN = "UNKNOWN";
+    public static List<String> allNames;
+    public static List<String> allCodes;
+    private static boolean initialized = false;
+
+    // Before using util.Language class, populate global lists of names/codes
+    private static void init() {
+        initialized = true;
+
+        // TODO: Read in names and codes
+        allNames = new ArrayList<>();
+        allCodes = new ArrayList<>();
+    }
+
+    public Language(String nm) {
+        if(!initialized)
+            init();
+
+        name = nm;
+        code = nm; // TODO: Change to code
+
+        // TODO: Validate language string
+    }
+
+    public Language() {
+        this(UNKNOWN);
+    }
+
+    public String toString() {
+        return name;
+    }
+}
