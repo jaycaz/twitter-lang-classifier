1c7d8cccd0922a2045ec40677307abd16ac07328
Martina Marek <martina@DN0a22a621.SUNet>
2015-11-10 10:10:16 -0800

    changed n gram

--- Language identification/src/classifier/NGramClassifier.java
+++ Language identification/src/classifier/NGramClassifier.java
@@ -10,33 +10,55 @@ import edu.stanford.nlp.classify.LinearClassifierFactory;
 import edu.stanford.nlp.ling.BasicDatum;
 import edu.stanford.nlp.ling.Datum;
 import edu.stanford.nlp.ling.RVFDatum;
-import edu.stanford.nlp.stats.IntCounter;
+import edu.stanford.nlp.stats.Counter;
+import edu.stanford.nlp.stats.Counters;
+import edu.stanford.nlp.stats.ClassicCounter;
 import util.Language;
 
-public class NGramClassifier extends Classifier {
+public class NGramClassifier {
 
-	LinearClassifier<IntCounter<String>, Language> c;
+	//LinearClassifier<ClassicCounter<String>, Language> c;
+	HashMap<Language, ClassicCounter<String>> nGramProb;
+	int nGram = 3;
+	int topCounts = 10000;
+	double minProb = 1/(topCounts * 10);
 	
-	@Override
 	public void train(
-			HashMap<Language, ArrayList<ArrayList<String>>> trainingData) {
-		GeneralDataset<IntCounter<String>, Language> dataSet=new Dataset<IntCounter<String>, Language>();
+			HashMap<Language, ArrayList<String>> trainingData) {
+		/*GeneralDataset<ClassicCounter<String>, Language> dataSet=new Dataset<ClassicCounter<String>, Language>();
 		for(Language language : trainingData.keySet()) {
-			for(ArrayList<String> paragraph : trainingData.get(language)) {
-				IntCounter<String> features = getFeatures(paragraph);
-				RVFDatum<IntCounter<String>, Language> d = new RVFDatum(features, language);
+			for(String sentence : trainingData.get(language)) {
+				ClassicCounter<String> features = getFeatures(sentence, nGram);
+				RVFDatum<ClassicCounter<String>, Language> d = new RVFDatum(features, language);
 				dataSet.add(d);
 			}
 		}
-		LinearClassifierFactory<IntCounter<String>, Language> lcFactory = new LinearClassifierFactory<IntCounter<String>, Language>();
-		c = lcFactory.trainClassifier(dataSet);
+		LinearClassifierFactory<ClassicCounter<String>, Language> lcFactory = new LinearClassifierFactory<ClassicCounter<String>, Language>();
+		c = lcFactory.trainClassifier(dataSet);*/
+		
+		for(Language language : trainingData.keySet()) {
+			nGramProb = new HashMap<Language, ClassicCounter<String>>();
+			ClassicCounter<String> features = new ClassicCounter<String>(); 
+			for (String sentence: trainingData.get(language)) {
+				features = countNGrams(sentence, features);
+			}
+			Counters.retainTop(features, topCounts);
+			Counters.normalize(features);
+			nGramProb.put(language, features);
+		}
 	}
 	
-	private IntCounter<String> getFeatures(ArrayList<String> sentence) {
-		IntCounter features = new IntCounter(); 
-		for (String word: sentence) {
-			features.incrementCount(word, 1);
+	private ClassicCounter<String> countNGrams(String sentence, ClassicCounter<String> counter) {
+		sentence.replace(" ", "_");
+		sentence = "_" + sentence + "_";
+		for (int i = 0; i < sentence.length() - nGram; i++) {
+			counter.incrementCount(sentence.substring(i, i + nGram), 1);
 		}
+		return counter;
+	}
+	
+	/*private ClassicCounter<String> getFeatures(ArrayList<String> sentences, int n) {
+		
 		return features;
 	}
 	
@@ -46,16 +68,55 @@ public class NGramClassifier extends Classifier {
 	
 	public void loadClassifier(String filename) {
 		c = LinearClassifier.readClassifier(filename);
-	}
+	}*/
 
-	@Override
-	public Language classify(ArrayList<String> sentence) {
-		IntCounter<String> features = getFeatures(sentence);
-		RVFDatum<IntCounter<String>, Language> d = new RVFDatum(features);
-		Object label = c.classOf(d);
-		return (Language) label;
+	public Language classify(String sentence) {
+		//ClassicCounter<String> features = getFeatures(sentence);
+		//RVFDatum<ClassicCounter<String>, Language> d = new RVFDatum(features);
+		//Object label = c.classOf(d);
+		//return (Language) label;
+		ClassicCounter<String> nGrams = new ClassicCounter<String>();
+		nGrams = countNGrams(sentence, nGrams);
+		double maxProb = -1;
+		Language maxLang = null;
+		for (Language lang: nGramProb.keySet()) {
+			double prob = 0;
+			ClassicCounter<String> langCounts = nGramProb.get(lang);
+			for (String s: nGrams.keySet()) {
+				double p = langCounts.getCount(s);
+				if (p == 0) {
+					prob = prob * minProb;
+				} else {
+					prob = p * nGrams.getCount(s);
+				}
+			}
+			if (prob > maxProb) {
+				maxProb = prob;
+				maxLang = lang;
+			}
+		}
+		if (maxLang == null) return Language.UNKNOWN;
+		return maxLang;
 	}
 	
-	
+	public double accuracy(HashMap<Language, ArrayList<String>> testSentences) {
+		int error = 0;
+		int total = 0;
+		HashMap<String, Double> performance = new HashMap<String, Double>();
+		for (Language lang: testSentences.keySet()) {
+			System.out.println("Processing lang: " + lang);
+			int i = 0;
+			for (String paragraph: testSentences.get(lang)) {
+				if (i > 100) continue;
+				Language guess = classify(paragraph);
+				if (!lang.equals(guess)) {
+					error++;
+				}
+				total++;
+				i++;
+			}
+		}
+		return (total - error) / (float) total;
+	}
 
 }
