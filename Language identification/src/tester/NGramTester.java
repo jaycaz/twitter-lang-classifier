369fd85735ea37aa84ef61f045f09788fcd52ff9
Martina Marek <martina@DN0a22a632.SUNet>
2015-11-19 10:25:12 -0800

    modified nGram Features so all n grams up to n can now be extracted

--- Language identification/src/tester/NGramTester.java
+++ Language identification/src/tester/NGramTester.java
@@ -3,6 +3,7 @@ package tester;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import dataReader.TwitterDataSimulator;
 import util.Language;
 import classifier.BaselineClassifier;
 import classifier.NGramClassifier;
@@ -22,6 +23,7 @@ public class NGramTester {
         classifier.train(TrainingData);
         //classifier.writeToFile("nGramClassifier");
         System.out.println("Finished Training. Now evaluating...!");
+        classifier.writeTopNFeaturesToFile("Top10NGrams.txt", 10);
         //HashMap<Language, ArrayList<ArrayList<String>>> devData = reader.getInputMap("_dev");
         //classifier.accuracyByClass(devData);
         //double accuracy = classifier.accuracy(devData);
@@ -29,7 +31,9 @@ public class NGramTester {
         //System.out.println("Acccuracy on dev: " + accuracy);
         //double fscore = classifier.f1(devData);
         //System.out.println("F1: " + fscore);
-        HashMap<Language, ArrayList<String>> testData = reader.getInputSentences("_test");
+        //HashMap<Language, ArrayList<String>> testData = reader.getInputSentences("_test");
+        TwitterDataSimulator twitterSim = new TwitterDataSimulator();
+        HashMap<Language, ArrayList<String>> testData = twitterSim.getTestingData(5, 10);
         double taccuracy = classifier.accuracy(testData);
         System.out.println("Acccuracy on test: " + taccuracy);
         //double tfscore = classifier.f1(testData);
