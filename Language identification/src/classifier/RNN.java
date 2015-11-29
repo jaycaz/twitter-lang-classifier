c04e53f5f8400bfdbc71b1e63fb1535584da622b
Martina Marek <martina@Martinas-MacBook-Pro.local>
2015-11-28 20:25:49 -0800

    first implementation of a RNN, still buggy

---
+++ Language identification/src/classifier/RNN.java
@@ -0,0 +1,160 @@
+package classifier;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Random;
+
+import dataReader.DataSetIterator;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.math3.util.Pair;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.distribution.UniformDistribution;
+import org.deeplearning4j.nn.conf.layers.GravesLSTM;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RNN {
+
+    private static Logger log = LoggerFactory.getLogger(RNN.class);
+    private static int total;
+    private static int error;
+
+
+    public static void main( String[] args ) throws Exception {
+        int lstmLayerSize = 500;					//Number of units in each GravesLSTM layer
+        int miniBatchSize = 30;						//Size of mini batch to use when  training
+        int examplesPerEpoch = 50*miniBatchSize;	//i.e., how many examples to learn on between generating samples
+        int exampleLength = 100;					//Length of each training example
+        int numEpochs = 100;							//Total number of training + sample generation epochs
+        int nSamplesToGenerate = 50;					//Number of samples to generate after each training epoch
+        Random rng = new Random(12345);
+
+        //Get a DataSetIterator that handles vectorization of text into something we can use to train
+        // our GravesLSTM network.
+        DataSetIterator iter = new DataSetIterator(exampleLength, miniBatchSize, examplesPerEpoch, new Random(12345));
+        int nOut = iter.totalOutcomes();
+
+        //Set up network configuration:
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).iterations(1)
+                .learningRate(0.1)
+                .rmsDecay(0.95)
+                .seed(12345)
+                .regularization(true)
+                .l2(0.001)
+                .list(3)
+                .layer(0, new GravesLSTM.Builder().nIn(iter.inputColumns()).nOut(lstmLayerSize)
+                        .updater(Updater.RMSPROP)
+                        .activation("tanh").weightInit(WeightInit.DISTRIBUTION)
+                        .dist(new UniformDistribution(-0.08, 0.08)).build())
+                .layer(1, new GravesLSTM.Builder().nIn(lstmLayerSize).nOut(lstmLayerSize)
+                        .updater(Updater.RMSPROP)
+                        .activation("tanh").weightInit(WeightInit.DISTRIBUTION)
+                        .dist(new UniformDistribution(-0.08, 0.08)).build())
+                .layer(2, new RnnOutputLayer.Builder(LossFunction.MCXENT).activation("softmax")        //MCXENT + softmax for classification
+                        .updater(Updater.RMSPROP)
+                        .nIn(lstmLayerSize).nOut(nOut).weightInit(WeightInit.DISTRIBUTION)
+                        .dist(new UniformDistribution(-0.08, 0.08)).build())
+                .pretrain(false).backprop(true)
+                .build();
+
+        MultiLayerNetwork net = new MultiLayerNetwork(conf);
+        net.init();
+        net.setListeners(new ScoreIterationListener(1));
+
+        //Print the  number of parameters in the network (and for each layer)
+        Layer[] layers = net.getLayers();
+        int totalNumParams = 0;
+        for( int i=0; i<layers.length; i++ ){
+            int nParams = layers[i].numParams();
+            System.out.println("Number of parameters in layer " + i + ": " + nParams);
+            totalNumParams += nParams;
+        }
+        System.out.println("Total number of network parameters: " + totalNumParams);
+
+        //Do training, and then generate and print samples from network
+        for( int i=0; i<numEpochs; i++ ){
+            DataSet next = iter.next();
+            net.fit(next);
+
+            System.out.println("--------------------");
+            System.out.println("Completed epoch " + i);
+            System.out.println("Trying to identify " + nSamplesToGenerate + " languages... ");
+            String[] samples = sampleLanguages(net,iter,rng,exampleLength,nSamplesToGenerate);
+            /*for( int j=0; j<samples.length; j++ ){
+                System.out.println("----- Sample " + j + " -----");
+                System.out.println(samples[j]);
+            }*/
+            System.out.println("----- Accuracy " + ((float) error)/total + " -----");
+            //iter.reset();	//Reset iterator for another epoch
+        }
+        System.out.println("\n\nExample complete");
+    }
+
+
+    private static String[] sampleLanguages(MultiLayerNetwork net,
+                                                         DataSetIterator iter, Random rng, int charactersToSample, int numSamples ){
+        //Create input for initialization
+
+        String[] labels = new String[numSamples];
+        String[] predicted = new String[numSamples];
+        for (int i = 0; i < numSamples; i++) {
+            Pair<String, String> ex = iter.getRandomSentence();
+            labels[i] = ex.getFirst();
+            INDArray initializationInput = Nd4j.zeros(new int[]{1, iter.inputColumns(), ex.getSecond().length()});
+            INDArray gold = Nd4j.zeros(new int[]{1, iter.totalOutcomes(), ex.getSecond().length()});
+            int langInd = iter.getLanguageIndex(ex.getFirst());
+            for (int j = 0; j < ex.getSecond().length(); j++) {
+                int ind;
+                if (ex.getSecond().length() <= j) ind = iter.getCharIndex(' ');
+                else ind = iter.getCharIndex(ex.getSecond().charAt(j));
+                if (ind == -1) ind = iter.getCharIndex(' ');
+                initializationInput.putScalar(new int[]{0,ind,j}, 1.0);
+                gold.putScalar(new int[]{0,langInd,j}, 1.0);
+            }
+            net.rnnClearPreviousState();
+            INDArray output = net.rnnTimeStep(initializationInput);
+            int maxInd = -1;
+            double max = Double.MIN_VALUE;
+            for (int k = 0; k < output.length(); k++) {
+                if (output.getDouble(k) > max) {
+                    max = output.getDouble(k);
+                    maxInd = k;
+                }
+            }
+            int index = maxInd/ex.getSecond().length();
+            predicted[i] = iter.getLanguageByIndex(index);
+        }
+
+        int locError = 0;
+        int locTotal = 0;
+        String[] out = new String[numSamples];
+        for( int i=0; i<numSamples; i++ ) {
+            out[i] = "True language: " + labels[i] + ", predicted: " + predicted[i];
+            if (labels[i] != predicted[i]) {
+                error++;
+                locError++;
+            }
+            total++;
+            locTotal++;
+        }
+        System.out.println("----- current accuracy " + ((float) locError)/locTotal + " -----");
+        return out;
+    }
+
+}
---
+++ Language identification/src/dataReader/DataSetIterator.java
@@ -0,0 +1,135 @@
+package dataReader;
+
+import org.apache.commons.math3.util.Pair;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import util.Language;
+import util.MapCreator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+/**
+ * Created by martina on 11/27/15.
+ *
+ * source: CharacterIterator from DeepLearning4j: http://deeplearning4j.org/recurrentnetwork.html
+ */
+public class DataSetIterator {
+
+    private String[] languages = {"afr", "deu", "fra"};
+    //private String[] languages = {"abk", "afr", "aka", "amh", "amu", "ara", "arg", "asm", "ast", "awa", "aym" ,"aze" ,"bam" ,"bel" ,"ben" ,"bih" ,"bis" ,"bos" ,"bpy" ,"bre" ,"bug" ,"bul" ,"cak" ,"cat" ,"cco" ,"ceb" ,"ces" ,"cha" ,"che" ,"chr" ,"chv" ,"ckb" ,"cor" ,"cos" ,"crh" ,"cym" ,"dan" ,"deu" ,"div" ,"dzo","ell" ,"eml" ,"eng" ,"epo" ,"est" ,"eus" ,"ewe" ,"fao" ,"fas" ,"fij" ,"fin" ,"fra" ,"frp" ,"fry" ,"ful" ,"gla" ,"gle" ,"glg" ,"glv" ,"grn" ,"guj" ,"hat" ,"hau" ,"haw" ,"heb", "hil", "hin","hrv" ,"hun" ,"hye" ,"ibo" ,"iku" ,"ilo" ,"ind" ,"isl" ,"jac" ,"jav" ,"jpn" ,"kab" ,"kal" ,"kan" ,"kat" ,"kaz" ,"kek" ,"khm" ,"kik" ,"kin" ,"kir" ,"kom" ,"kor" ,"kur" ,"lad" ,"lao" ,"lat" ,"lav" ,"lez" ,"lij" ,"lin" ,"lit" ,"lmo" ,"ltz" ,"lug" ,"mal" ,"mam" ,"mar" ,"min" ,"mkd" ,"mlg" ,"mlt" ,"mon" ,"mri" ,"msa","mya" ,"mzn" ,"nah" ,"nap" ,"nav" ,"ndo" ,"nds" ,"nep" ,"new" ,"nld" ,"nno" ,"nob" ,"nor" ,"nya" ,"oci" ,"ori" ,"orm" ,"pam" ,"pan" ,"pdc" ,"pdt" ,"pms" ,"pol" ,"por" ,"ppl" ,"pus" ,"quc" ,"que" ,"roh" ,"ron" ,"rus" ,"scn" ,"sco" ,"sin" ,"slk" ,"slv" ,"sme" ,"smo" ,"sna" ,"snd" ,"som" ,"spa" ,"sqi" ,"srd" ,"srp" ,"sun" ,"swa" ,"swe" ,"tah" ,"tam" ,"tat" ,"tel" ,"tgk" ,"tgl" ,"tha" ,"tir" ,"ton" ,"tpi" ,"tsn" ,"tum" ,"tur" ,"twi" ,"udm" ,"uig" ,"ukr" ,"urd" ,"usp" ,"uzb" ,"vec" ,"ven" ,"vie" ,"vol" ,"war" ,"wln" ,"wol" ,"xal" ,"xho" ,"yid" ,"yor" ,"zh-yue" ,"zha" ,"zho" ,"zul" };
+    private HashMap<String, Integer> languageToIndMap;
+    private char[] uniqueChars;
+    private HashMap<Character, Integer> charToIndMap;
+    private int exampleLength;
+    private int miniBatchSize;
+    private int numExamplesToFetch;
+    //private int examplesSoFar = 0;
+    private int currLanguageInd = 0;
+    private Random rng;
+    HashMap<Language, ArrayList<String>> trainingData;
+
+    public DataSetIterator (int exampleLength, int miniBatchSize, int numExamplesToFetch, Random rng) {
+        this.exampleLength = exampleLength;
+        this.miniBatchSize = miniBatchSize;
+        this.numExamplesToFetch = numExamplesToFetch;
+        this.rng = rng;
+        languageToIndMap = new HashMap<>();
+        for (int i = 0; i < languages.length; i++) languageToIndMap.put(languages[i], i);
+        MapCreator createUniqueChars = new MapCreator();
+        String[] filenames = new String[languages.length];
+        for (int i = 0; i < languages.length; i++) filenames[i] = languages[i] + ".txt";
+        charToIndMap = createUniqueChars.createUniqueCharacterMap("data/", filenames);
+        uniqueChars = new char[charToIndMap.size()];
+        for (char c: charToIndMap.keySet()) uniqueChars[charToIndMap.get(c)] = c;
+        ReadData reader = new ReadData();
+        trainingData = reader.getInputSentences("_train");
+    }
+
+    //public boolean hasNext() {
+       // return examplesSoFar + miniBatchSize <= numExamplesToFetch;
+    //}
+
+    public DataSet next() {
+        return next(miniBatchSize);
+    }
+
+    public DataSet next(int num) {
+        //if (examplesSoFar + num > numExamplesToFetch) throw new NoSuchElementException();
+        //Allocate space:
+        INDArray input = Nd4j.zeros(new int[]{num, uniqueChars.length, exampleLength});
+        INDArray labels = Nd4j.zeros(new int[]{num, languages.length, exampleLength});
+
+        for (int i = 0; i < num; i++) {
+            Language l = new Language(languages[currLanguageInd]);
+            ArrayList<String> list = new ArrayList<>();
+            for (Language ll: trainingData.keySet()) {
+                if (ll.equals(l)) {
+                    list = trainingData.get(ll);
+                }
+            }
+            int randSentence = rng.nextInt(list.size());
+            String sentence = list.get(randSentence);
+            for (int j = 0; j < exampleLength; j++) {
+                char nextChar;
+                if (j >= sentence.length()) nextChar = ' ';
+                else nextChar = sentence.charAt(j);
+                int charInd = getCharIndex(nextChar);
+                if (charInd == -1) charInd = getCharIndex(' ');
+                input.putScalar(new int[]{i,charInd,j}, 1.0);
+                labels.putScalar(new int[]{i,currLanguageInd,j}, 1.0);
+            }
+            incrementLangaugeInd(); //just take one example of each language, then move on to next language
+        }
+
+        //examplesSoFar += num;
+        return new DataSet(input,labels);
+    }
+
+    private void incrementLangaugeInd() {
+        if (currLanguageInd < languages.length - 1) currLanguageInd++;
+        else currLanguageInd = 0;
+    }
+
+    public int getLanguageIndex (String language) {return languageToIndMap.get(language);}
+
+    public String getLanguageByIndex (int index) {return languages[index];}
+
+    public int getCharIndex (Character c) {
+        if (!charToIndMap.containsKey(c)) return -1;
+        return charToIndMap.get(c);
+    }
+
+    public Character getCharByIndex (int index) {return uniqueChars[index];}
+
+    public int inputColumns() {
+        return uniqueChars.length;
+    }
+
+    public int totalOutcomes() {
+        return languages.length;
+    }
+
+    //public void reset() {
+        //examplesSoFar = 0;
+    //}
+
+    public Pair<String, String> getRandomSentence() {
+        int randLang = rng.nextInt(languages.length);
+        String lang = languages[randLang];
+        Language l = new Language(lang);
+        ArrayList<String> list = new ArrayList<>();
+        for (Language ll: trainingData.keySet()) {
+            if (ll.equals(l)) {
+                list = trainingData.get(ll);
+            }
+        }
+        int randSentence = rng.nextInt(list.size());
+        String sent = list.get(randSentence);
+        return new Pair<String, String>(lang, sent);
+    }
+
+}
