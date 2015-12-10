f37f2c13bcfc387b29fbd04a44e2e155755d2850
Martina Marek <martina@DN525bgn.SUNet>
2015-12-09 18:49:13 -0800

    small changes

--- Language identification/src/main/java/org/dataReader/ReadData.java
+++ Language identification/src/main/java/org/dataReader/ReadData.java
@@ -1,9 +1,9 @@
 /**
  * Created by May on 10/20/15.
  */
-package dataReader;
+package org.dataReader;
 
-import util.Language;
+import org.util.Language;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
